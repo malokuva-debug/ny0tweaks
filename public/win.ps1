0c6758f3b4806ff4e6b3a71d0d33499037c8eb75
malokuva-debug <malokuva@gmail.com>
2026-03-17 13:48:53 +0100

    Update win.ps1

--- public/win.ps1
+++ public/win.ps1
@@ -1,2 +1,5 @@
 [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
-iwr -useb https://ny0tweaks.vercel.app/Win11-Gaming-Optimizer-GUI.ps1 | iex
+$url = "https://ny0tweaks.vercel.app/Win11-Gaming-Optimizer-GUI.ps1"
+$tmp = "$env:TEMP\Win11-Gaming-Optimizer-GUI.ps1"
+(New-Object System.Net.WebClient).DownloadFile($url, $tmp)
+& $tmp
